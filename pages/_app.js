886ba089cd0cd9ebe6d6af0e488f75c4dddf71f5
minhazurrony <minhazur.rony@gmail.com>
2020-12-15 16:01:28 +0600

    chore: initial project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/antd.less';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'antd';
+
+const Home = () => {
+  return <Button type="primary">Click to Fuck Me</Button>;
+};
+
+export default Home;
